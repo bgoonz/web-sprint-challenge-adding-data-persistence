31586f6a66af116c0d535fa7db66e6abf97e7963
bryan <bryan.guner@gmail.com>
2021-07-16 14:11:11 -0400

    setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const server = require('./api/server')
+const port = process.env.PORT || 5000 
+
+server.listen(port, '0.0.0.0', () => {
+    console.log("Listening on port:", port)
+})
